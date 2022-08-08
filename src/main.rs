666217ca25937c6794cc46fd5df4403fd24737d5
Cody Bloemhard <cblmhrd@live.nl>
2022-08-08 19:31:56 +0200

    added: example toml and deserializing;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use toml::Value;
+use serde::Deserialize;
+
+use std::{ fs };
+
+#[derive(Deserialize, Debug)]
+struct Chapter{
+    manga: String,
+    author: String,
+    volume: usize,
+    chapter: String,
+    pic: Vec<Picture>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Picture{
+    nr: usize,
+    page: usize,
+    characters: Vec<String>,
+    location: Option<String>,
+    text: Vec<Text>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Text{
+    from: String,
+    to: Option<String>,
+    lines: Vec<String>,
+    kmap: Option<Vec<[String; 2]>>,
+    transl: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("example.toml").expect("oof");
+    let chapter: Chapter = toml::from_str(&contents).expect("auw");
+    println!("{:#?}", chapter);
 }
