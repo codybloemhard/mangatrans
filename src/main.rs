1d4f8bfb68576869429553c5c1688b6d035f7a15
Cody Bloemhard <cblmhrd@live.nl>
2022-08-16 01:23:56 +0200

    added: characters and speaking time related stats;

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ struct Args{
     log: bool,
     #[clap(short='d', long, value_parser)]
     outputdir: Option<PathBuf>,
-    #[clap(short, long, value_parser)]
+    #[clap(required = true)]
     inputfiles: Vec<PathBuf>,
 }
 
@@ -105,32 +105,58 @@ struct Stats{
     pictures: usize,
     morae: usize,
     locations: HashMap<String, (usize, usize)>,
+    characters: HashMap<String, usize>,
+    speaks: HashMap<String, usize>,
+    spoken_to: HashMap<String, usize>,
+    conversation_pair: HashMap<String, usize>,
 }
 
-fn stats_report(s: Stats, doc: &mut String){
+fn stats_report(mut s: Stats, doc: &mut String){
     let _ = writeln!(doc, "Manga: {}", s.manga);
     let _ = write!(doc, "Volumes: ");
+
+    s.volumes.sort();
+    s.volumes.dedup();
     for vol in s.volumes{
         let _ = write!(doc, "{}, ", vol);
     }
     doc.pop();
     doc.pop();
     let _ = writeln!(doc);
+
     let _ = write!(doc, "Chapters: ");
+    s.chapters.sort();
+    s.chapters.dedup();
     for chap in s.chapters{
         let _ = write!(doc, "{}, ", chap);
     }
     doc.pop();
     doc.pop();
     let _ = writeln!(doc);
+
     let _ = writeln!(doc, "Pictures: {}", s.pictures);
     let _ = writeln!(doc, "Morae spoken: {}", s.morae);
+
     let _ = writeln!(doc, "Locations: ");
     let mut locs = s.locations.into_iter().collect::<Vec<_>>();
     locs.sort_unstable_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
     for (name, (count, morae)) in locs{
         let _ = writeln!(doc, "\t{}: {} appearances, {} morae spoken in.", name, count, morae);
     }
+
+    let mut write_list = |hmap: HashMap<String, usize>, title: &str|{
+        let _ = writeln!(doc, "{}", title);
+        let mut list = hmap.into_iter().collect::<Vec<_>>();
+        list.sort_unstable_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
+        for (name, count) in list{
+            let _ = writeln!(doc, "\t{}: {}", name, count);
+        }
+    };
+
+    write_list(s.characters, "Character appearances: ");
+    write_list(s.speaks, "Morae spoken: ");
+    write_list(s.spoken_to, "Morae spoken to: ");
+    write_list(s.conversation_pair, "Conversation pairs in Morae: ");
 }
 
 fn accumulate_stats(chapter: Chapter, stats: &mut Stats, log: &mut String){
@@ -144,7 +170,7 @@ fn accumulate_stats(chapter: Chapter, stats: &mut Stats, log: &mut String){
 
     if stats.manga.is_empty(){
         stats.manga = chapter.manga;
-    } else {
+    } else if stats.manga != chapter.manga{
         let _ = writeln!(log, "Different manga found: {}. Current manga is: {}.",
                          chapter.manga, stats.manga);
     }
@@ -154,6 +180,11 @@ fn accumulate_stats(chapter: Chapter, stats: &mut Stats, log: &mut String){
         stats.pictures += 1;
         let location = picture.location.unwrap_or_default();
         let mut pic_morae = 0;
+        if let Some(characters) = picture.characters{
+            for character in characters{
+                update(&mut stats.characters, &character, 1, |a, b| a + b);
+            }
+        }
         if let Some(texts) = picture.text{
             for text in texts{
                 let replacements = if let Some(kmap) = &text.kmap{
@@ -173,6 +204,16 @@ fn accumulate_stats(chapter: Chapter, stats: &mut Stats, log: &mut String){
                     .fold(0, |acc, c| acc + to_mora(c));
                 stats.morae += morae;
                 pic_morae += morae;
+                update(&mut stats.speaks, &text.from, morae, |a, b| a + b);
+                if let Some(receiver) = text.to{
+                    let pair = if text.from.cmp(&receiver) == std::cmp::Ordering::Less{
+                        format!("{}, {}", text.from, receiver)
+                    } else {
+                        format!("{}, {}", receiver, text.from)
+                    };
+                    update(&mut stats.spoken_to, &receiver, morae, |a, b| a + b);
+                    update(&mut stats.conversation_pair, &pair, morae, |a, b| a + b);
+                }
             }
         };
         update(&mut stats.locations, &location, (1, pic_morae), |(a, b), (c, d)| (a + c, b + d));
